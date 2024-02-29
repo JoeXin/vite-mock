1c8e44a516fe9e1b976a0dbbc53b2c68b7714741
JoeXin <1203970535@qq.com>
2024-02-29 15:49:44 +0800

    修改默认路径

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  
+  base: '/vite-mock',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
